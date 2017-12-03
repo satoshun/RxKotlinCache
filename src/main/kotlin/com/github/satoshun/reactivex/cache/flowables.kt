5eac70d8c80ee23aa57a38dae9b8c8f13244e330
SatoShun <shun.sato1@gmail.com>
2017-12-03 05:11:29 +0000

    Flowable + Function3

--- src/main/kotlin/com/github/satoshun/reactivex/cache/flowables.kt
+++ src/main/kotlin/com/github/satoshun/reactivex/cache/flowables.kt
@@ -46,3 +46,26 @@ class FlowableCache2<in P1, in P2, R>(
         .doOnComplete { cache[key] = cacheList }
   }
 }
+
+fun <P1, P2, P3, R> rxCache(original: Function3<P1, P2, P3, Flowable<R>>): FlowableCache3<P1, P2, P3, R> {
+  return FlowableCache3(original)
+}
+
+class FlowableCache3<in P1, in P2, in P3, R>(
+    private val original: Function3<P1, P2, P3, Flowable<R>>
+) : Function3<P1, P2, P3, Flowable<R>> {
+  private val cache = CacheMap<CacheKey3<P1, P2, P3>, CacheValueList<R>>()
+
+  override operator fun invoke(p1: P1, p2: P2, p3: P3): Flowable<R> {
+    val value = cache[CacheKey3(p1, p2, p3)]
+    return if (value != null) Flowable.fromIterable(value)
+    else forceInvalidation(p1, p2, p3)
+  }
+
+  fun forceInvalidation(p1: P1, p2: P2, p3: P3): Flowable<R> {
+    val key = CacheKey3(p1, p2, p3)
+    val cacheList = CacheValueList<R>()
+    return original(p1, p2, p3).doOnNext { cacheList += it }
+        .doOnComplete { cache[key] = cacheList }
+  }
+}
