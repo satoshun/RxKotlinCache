0630d94dd024c26347389a33417eadf118f86773
SatoShun <shun.sato1@gmail.com>
2017-12-03 09:33:19 +0000

    Maybe + Function2

--- src/main/kotlin/com/github/satoshun/reactivex/cache/maybes.kt
+++ src/main/kotlin/com/github/satoshun/reactivex/cache/maybes.kt
@@ -23,6 +23,28 @@ class MaybeCache1<in P1, R : Any>(
   }
 }
 
+fun <P1, P2, R : Any> rxCache(original: Function2<P1, P2, Maybe<R>>): MaybeCache2<P1, P2, R> {
+  return MaybeCache2(original)
+}
+
+class MaybeCache2<in P1, in P2, R : Any>(
+    private val original: Function2<P1, P2, Maybe<R>>
+) : Function2<P1, P2, Maybe<R>> {
+  private val cache = CacheMap<CacheKey2<P1, P2>, Either<R, MaybeComplete>>()
+
+  override operator fun invoke(p1: P1, p2: P2): Maybe<R> {
+    val value = cache[CacheKey2(p1, p2)]
+    return value?.toMaybe() ?: forceInvalidation(p1, p2)
+  }
+
+  fun forceInvalidation(p1: P1, p2: P2): Maybe<R> {
+    val key = CacheKey2(p1, p2)
+    return original(p1, p2)
+        .doOnSuccess { cache[key] = Either.left(it) }
+        .doOnComplete { cache[key] = Either.right(MaybeComplete) }
+  }
+}
+
 private fun <R : Any> Either<R, MaybeComplete>.toMaybe(): Maybe<R> {
   return if (left != null) Maybe.just(left)
   else Maybe.empty()
--- src/test/kotlin/com/github/satoshun/reactivex/cache/maybesTest.kt
+++ src/test/kotlin/com/github/satoshun/reactivex/cache/maybesTest.kt
@@ -71,4 +71,67 @@ class MaybesTest {
     wrapped(1000).test().assertError(error)
     assertThat(counter, `is`(2000))
   }
+
+  @Test
+  fun cache2_success() {
+    var counter = 0
+    val original = object : Function2<Int, Int, Maybe<String>> {
+      override fun invoke(p1: Int, p2: Int): Maybe<String> {
+        counter += p1 + p2
+        return Maybe.just(p1.toString())
+      }
+    }
+    val wrapped = rxCache(original)
+
+    wrapped(1, 2).test().assertValue("1")
+    assertThat(counter, `is`(3))
+    wrapped(2, 3).test().assertValue("2")
+    assertThat(counter, `is`(8))
+
+    wrapped(1, 2).test().assertValue("1")
+    assertThat(counter, `is`(8))
+
+    wrapped.forceInvalidation(1, 2).test().assertValue("1")
+    assertThat(counter, `is`(11))
+  }
+
+  @Test
+  fun cache2_complete() {
+    var counter = 0
+    val original = object : Function2<Int, Int, Maybe<String>> {
+      override fun invoke(p1: Int, p2: Int): Maybe<String> {
+        counter += p1 + p2
+        return Maybe.empty()
+      }
+    }
+    val wrapped = rxCache(original)
+
+    wrapped(1, 2).test().assertComplete()
+    assertThat(counter, `is`(3))
+    wrapped(2, 3).test().assertComplete()
+    assertThat(counter, `is`(8))
+
+    wrapped(1, 2).test().assertComplete()
+    assertThat(counter, `is`(8))
+
+    wrapped.forceInvalidation(1, 2).test().assertComplete()
+    assertThat(counter, `is`(11))
+  }
+
+  @Test
+  fun cache2_exception() {
+    var counter = 0
+    val original = object : Function2<Int, Int, Maybe<String>> {
+      override fun invoke(p1: Int, p2: Int): Maybe<String> {
+        counter += p1 + p2
+        return Maybe.error(error)
+      }
+    }
+    val wrapped = rxCache(original)
+
+    wrapped(1, 2).test().assertError(error)
+    assertThat(counter, `is`(3))
+    wrapped(1, 2).test().assertError(error)
+    assertThat(counter, `is`(6))
+  }
 }
